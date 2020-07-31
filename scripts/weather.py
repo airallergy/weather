11c7181445d59ac00590a4c39b3806aec783bfc1
airallergy <49818327+airallergy@users.noreply.github.com>
2020-07-31 23:04:42 +0100

    Update weather.py

--- scripts/weather.py
+++ scripts/weather.py
@@ -4,7 +4,7 @@ import re
 import pandas as pd
 import numpy as np
 
-from typing import List, Union
+from typing import List, Tuple, Union
 from os import PathLike
 
 AnyPath = Union[str, bytes, PathLike]
@@ -16,10 +16,11 @@ with open("scripts/epw_scheme.json", "rt") as fh:
 class Epw:
     def __init__(self, epw_file: AnyPath) -> None:
         self.epw_file = epw_file
+        self.headers = tuple(EPW_SCHEME.keys())
         self.header = self._convert_header(self.__class__.__name__, "var")
 
     def __getattr__(self, name: str) -> object:
-        if name in EPW_SCHEME.keys():
+        if name in self.headers:
 
             header_cls_name = self._convert_header(name, "cls")
             return globals()[header_cls_name](self.epw_file)
@@ -49,18 +50,24 @@ class Epw:
                 "sanity check failed for {}".format(self.__class__.__name__)
             )
 
-    def read_entry(self, header: str) -> Union[List[str], str]:
-        header = header.lower()
-        headers = tuple(EPW_SCHEME.keys())
-        if header not in headers:
+    def _read_scheme(self, category: str) -> Tuple:
+        fields = EPW_SCHEME[self.header][category]["fields"]
+        types = EPW_SCHEME[self.header][category]["types"]
+        if category == "data":
+            count_idx = EPW_SCHEME[self.header][category]["count_idx"]
+            return fields, types, count_idx
+        return fields, types
+
+    def _read_entry(self) -> Union[List[str], str]:
+        if self.header not in self.headers:
             raise ValueError("invalid header")
-        start_ln_no = headers.index(header)
+        start_ln_no = self.headers.index(self.header)
         with open(self.epw_file, "rt") as fh:
             epw = [line.rstrip() for line in fh.readlines()]
-        if header == headers[-1]:
+        if self.header == self.headers[-1]:
             entry = epw[start_ln_no:]
         else:
-            entry = epw[start_ln_no].split(header + ",")[-1].strip()
+            entry = epw[start_ln_no].split(self.header + ",")[-1].strip()
         return entry
 
 
@@ -73,7 +80,7 @@ class GroundTemperatures(Epw):
     def parse_metadata(self):
         fields = EPW_SCHEME[self.header]["metadata"]["fields"]
         types = EPW_SCHEME[self.header]["metadata"]["types"]
-        entry = self.read_entry(self.header)
+        entry = self._read_entry()
         entry = entry.split(",")[1:]
         metadata = pd.DataFrame(dict(zip(fields, entry[: len(fields)])), index=[0])
         metadata = metadata.astype(dict(zip(fields, types)))
@@ -83,7 +90,7 @@ class GroundTemperatures(Epw):
     def parse_data(self):
         fields = EPW_SCHEME[self.header]["data"]["fields"]
         types = EPW_SCHEME[self.header]["data"]["types"]
-        count_idx = EPW_SCHEME[self.header]["data"]["count"]
+        count_idx = EPW_SCHEME[self.header]["data"]["count_idx"]
         count = getattr(self, count_idx)
         self._check_sanity(self.entry_data, fields, count)
         data = pd.DataFrame(
@@ -102,11 +109,12 @@ class Records(Epw):
         self.data = self.parse_data()
 
     def parse_data(self) -> pd.DataFrame:
-        entry = self.read_entry(self.header)
+        fields, types, _ = self._read_scheme("data")
+        entry = self._read_entry()
         data = pd.DataFrame(item.split(",") for item in entry)
         data.replace("", np.nan, inplace=True)
-        fields = EPW_SCHEME[self.header]["fields"][: data.shape[1]]
-        types = EPW_SCHEME[self.header]["types"][: data.shape[1]]
+        fields = fields[: data.shape[1]]
+        types = types[: data.shape[1]]
         data.columns = fields
         data = data.astype(dict(zip(fields, types)))
         timestamp = data.apply(
@@ -127,5 +135,5 @@ class Records(Epw):
 p = Path("scripts") / "in.epw"
 w = Epw(p)
 # b = tuple(EPW_SCHEME.keys())
-# print(w.records.dry_bulb_temperature)
-print(w.ground_temperatures.soil_conductivity)
+print(w.records.dry_bulb_temperature)
+# print(w.ground_temperatures.soil_conductivity)
