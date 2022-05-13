e848352575a7a999977d2eef8e8a602eb6e0cadb
Cheng Cui <cheng.cui.95@gmail.com>
2022-05-13 14:34:38 +0100

    Refactor weather scripts: fix field schema type hint

--- scripts/pyphd/weather/_tools.py
+++ scripts/pyphd/weather/_tools.py
@@ -1,6 +1,6 @@
 from os import PathLike
 
 AnyPath = str | bytes | PathLike[str] | PathLike[bytes]
-AnyFieldSchema = tuple[str | type]
+AnyFieldSchema = dict[str:type]
 AnyField = int | float | str
 AnyRecords = tuple[tuple[AnyField]]
